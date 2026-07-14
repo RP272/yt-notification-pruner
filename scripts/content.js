2b825dd9c061fc8fd1348a0563b0ba94527d2ba3
RP272 <272931@student.pwr.edu.pl>
2026-07-14 10:33:11 +0200

    remove shorts notifications

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,20 @@
-// TODO: remove shorts notifications from YouTube
+const observer = new MutationObserver(async (mutations) => {
+    for (const mutation of mutations) {
+        for (const node of mutation.addedNodes) {
+            if (!(node instanceof HTMLElement))
+                continue;
+
+            if (node.matches("ytd-notification-renderer")) {
+                const link = node.querySelector("a");
+
+                if (link?.href.includes("/shorts/")) {
+                    node.remove();
+                }
+
+            }
+        }
+    }
+});
+
+const popupContainer = document.querySelector("ytd-popup-container");
+observer.observe(popupContainer, { childList: true, subtree: true });
